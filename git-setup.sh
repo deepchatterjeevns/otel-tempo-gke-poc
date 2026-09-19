37278221bc3de24bbbc7f8a884591642857bf6a5
deepchatterjeevns <deepchatterjee.pune@gmail.com>
2026-09-19 22:53:16 +0530

    Initial commit

--- git-setup.sh
+++ git-setup.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-# Git setup script for otel-tempo-gke-poc
-
 REMOTE_URL=""
 BRANCH_NAME="main"
 COMMIT_MESSAGE="Initial commit"
 GITHUB_USER="deepchatterjeevns"
 REPO_NAME="otel-tempo-gke-poc"
-
 while getopts "r:b:m:" opt; do
   case $opt in
     r) REMOTE_URL="$OPTARG" ;;
@@ -15,17 +12,12 @@ while getopts "r:b:m:" opt; do
     *) echo "Usage: $0 [-r remote_url] [-b branch_name] [-m commit_message]"; exit 1 ;;
   esac
 done
-
-# Default remote URL if not provided
 if [ -z "$REMOTE_URL" ]; then
   REMOTE_URL="https://github.com/${GITHUB_USER}/${REPO_NAME}.git"
 fi
-
 git init
 git add -A
 git commit -m "$COMMIT_MESSAGE"
-
 git remote add origin "$REMOTE_URL" 2>/dev/null || git remote set-url origin "$REMOTE_URL"
 git push -u origin "$BRANCH_NAME"
-
 echo "Git repository initialized and pushed to $REMOTE_URL"
